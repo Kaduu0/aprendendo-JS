5a0838b09f080b1bd42fdc92b95ad2e406565ca6
kadu <77140345+kaduu21@users.noreply.github.com>
2024-12-12 14:40:08 -0300

    save de progreço
    Aprendi o básico EBAA!!!

---
+++ alertas e linkagem/main.js
@@ -0,0 +1,5 @@
+window.alert ('OLA MUNDO') /*cria um alerta com mensagem pre-definida*/
+
+window.confirm ('esse é o primeiro contato') /*cria um dialogo de confirmação, pode ser configurado*/
+
+window.prompt ('escreva seu nome!') /*prompt é capaz de colhetar dados para criar uma interação*/
